e83b02726b5c7507c63ef73025f51779f11a835f
SRIKANTH MU <srik19414.ec@rmkec.ac.in>
2024-02-26 19:10:00 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,6 +1,18 @@
 import {FormControl,FormModule,ReactiveFormsModule} from '@angular/forms';
 
 imports:[
-  CommonModule,ReactiveFormsModule
+  CommonModule,
+  ReactiveFormsModule
 ],
-  
+  applyForm=new Form Group({
+  firstName: new FormControl(''),
+  lastName: new FormControl(''),
+  email: new FormControl('')
+});
+submitApplication() {
+  this.housingService.submitApplication(
+    this.applyForm.value.firstName ?? '',
+    this.applyForm.value.lastName ?? '',
+    this.applyForm.value.email ?? ''
+  );
+}
